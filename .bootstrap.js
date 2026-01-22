a00674ffc4371aaa3cdd25ab0180de9531073525
Vibesdk <vibesdk-bot@cloudflare.com>
2026-01-22 04:14:26 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @fontsource/playfair-display@^5.1.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
